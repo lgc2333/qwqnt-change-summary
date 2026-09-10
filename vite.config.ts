dbd22310ea4b1baf38abe26e1bbd4cbeef1d7d81
LgCuwukii☆ <lgc2333@126.com>
2026-09-11 00:22:52 +0800

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,99 @@
+import { defineConfig } from 'vite';
+import { resolve } from 'node:path';
+import { builtinModules } from 'node:module';
+import viteCp from 'vite-plugin-cp';
+import viteOxlint from 'unplugin-oxlint/vite';
+import viteZipPack from 'unplugin-zip-pack/vite';
+import Plugin from './package.json';
+
+const SRC_DIR = resolve(__dirname, './src');
+const OUTPUT_DIR = resolve(__dirname, './dist');
+
+const external = ['electron', ...builtinModules.flatMap(m => [m, `node:${m}`])];
+
+const BaseConfig = defineConfig({
+  root: __dirname,
+  resolve: {
+    alias: {
+      '@': SRC_DIR,
+    },
+  },
+});
+
+const configs = {
+  main: defineConfig({
+    ...BaseConfig,
+
+    plugins: [
+      viteOxlint({
+        fix: true,
+      }),
+    ],
+    build: {
+      minify: true,
+      outDir: resolve(OUTPUT_DIR, './main'),
+      lib: {
+        entry: resolve(SRC_DIR, './main/index.ts'),
+        formats: [ 'es' ],
+
+        fileName: () => 'index.js',
+      },
+      rolldownOptions: {
+        external,
+      },
+      target: 'node23',
+    },
+  }),
+  preload: defineConfig({
+    ...BaseConfig,
+
+    plugins: [
+      viteOxlint({
+        fix: true,
+      }),
+    ],
+    build: {
+      minify: true,
+      outDir: resolve(OUTPUT_DIR, './preload'),
+      lib: {
+        entry: resolve(SRC_DIR, './preload/index.ts'),
+        formats: [ 'cjs' ],
+
+        fileName: () => 'index.cjs',
+      },
+      rolldownOptions: {
+        external,
+      },
+    },
+  }),
+  renderer: defineConfig({
+    ...BaseConfig,
+
+    plugins: [
+      viteOxlint({
+        fix: true,
+      }),
+      viteCp({
+        targets: [{ src: './package.json', dest: 'dist' }],
+      }),
+      viteZipPack({
+        in: OUTPUT_DIR,
+        out: resolve(__dirname, `./${Plugin.name}.zip`),
+      }),
+    ],
+    build: {
+      minify: true,
+      outDir: resolve(OUTPUT_DIR, './renderer'),
+      lib: {
+        entry: resolve(SRC_DIR, './renderer/index.ts'),
+        formats: [ 'es' ],
+        fileName: () => 'index.js',
+      },
+      rolldownOptions: {
+        external,
+      },
+    },
+  }),
+};
+
+export default defineConfig(({ mode }) => configs[mode as keyof typeof configs]);
